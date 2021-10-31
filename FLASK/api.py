81e6e4fa5692ed57d54d07f2c5df02427f8e6b47
John Esteban Alvarez Piedrahita <esteban.ea145@gmail.com>
2021-10-31 00:56:56 -0500

    sesión 11

--- FLASK/api.py
+++ FLASK/api.py
@@ -1,13 +1,45 @@
-from flask import Flask,redirect,url_for,render_template,request
+from flask import Flask,redirect,url_for,render_template,request,jsonify
 
 app=Flask(__name__)
 @app.route('/',methods=['GET','POST'])
 def home():
-    if request.method=='POST':
-        # Handle POST Request here
-        return render_template('index.html')
-    return render_template('index.html')
+   return "<h1>Mi primer api de peliculas</h1>"
+
+
+#Simulador de Db de peliculas
+peliculas=[
+    {"id":0,
+    "name":"El padrino",
+    "estreno":1972},
+    {"id":1,
+    "name":"V De venganza",
+    "estreno":1999},
+]
+
+@app.route('/api/peliculas/todas', methods=['GET'])
+def api_peliculas_todas():
+    return jsonify(peliculas)
+
+
+@app.route('/api/peliculas/', methods=['GET'])
+def api_peliculas():
+    if ("id" in request.args):
+        id = int(request.args["id"])
+    else:
+        return "Error: No se ingrese ningun ID"
+    
+    for pelicula in peliculas:
+        if (pelicula["id"] == id):
+            resultado = pelicula
+            break
+
+    return jsonify(resultado)
+
 
 if __name__ == '__main__':
     #DEBUG is SET to TRUE. CHANGE FOR PROD
     app.run(port=5000,debug=True)
+
+
+
+
